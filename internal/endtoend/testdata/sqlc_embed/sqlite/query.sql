6cbf92d3abc706f99fcb3b84de8b3dd0a29187d0
Antoine GIRARD <antoine.girard@sapk.fr>
2023-09-03 23:11:36 +0200

    regroup embed tests

--- internal/endtoend/testdata/sqlc_embed/sqlite/query.sql
+++ internal/endtoend/testdata/sqlc_embed/sqlite/query.sql
@@ -16,6 +16,11 @@ CREATE TABLE baz.users (
     name text NOT NULL
 );
 
+CREATE TABLE user_links (
+  owner_id integer NOT NULL,
+  consumer_id integer NOT NULL,
+  PRIMARY KEY (owner_id, consumer_id)
+);
 
 -- name: Only :one
 SELECT sqlc.embed(users) FROM users;
@@ -42,3 +47,12 @@ SELECT sqlc.embed(bu) FROM baz.users AS bu;
 -- name: WithCrossSchema :many
 SELECT sqlc.embed(u), sqlc.embed(bu) FROM users AS u
 INNER JOIN baz.users bu ON u.id = bu.id;
+
+-- name: ListUserLink :many
+SELECT
+    sqlc.embed(owner),
+    sqlc.embed(consumer)
+FROM
+    user_links
+    INNER JOIN users AS owner ON owner.id = user_links.owner_id
+    INNER JOIN users AS consumer ON consumer.id = user_links.consumer_id;
